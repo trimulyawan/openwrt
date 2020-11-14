1acf15bdd23a09a4a66390d6a47163b4cd73c9c6
Marty Jones <mj8263788@gmail.com>
2020-11-14 13:02:25 -0500

    kernel: bump 5.9 to 5.9.8
    Signed-off-by: Marty Jones <mj8263788@gmail.com>

--- include/kernel-version.mk
+++ include/kernel-version.mk
@@ -7,10 +7,10 @@ ifdef CONFIG_TESTING_KERNEL
 endif
 
 LINUX_VERSION-5.4 = .77
-LINUX_VERSION-5.9 = .6
+LINUX_VERSION-5.9 = .8
 
 LINUX_KERNEL_HASH-5.4.77 = a3e03e6970240dddc8174bf9f49b56d774c40125eabe1582d2ebe85b01addbf7
-LINUX_KERNEL_HASH-5.9.6 = 6cc182fe19a8f3958c24d453f4ab68eb73fb5cfb8af4c95b02f3db049e655370
+LINUX_KERNEL_HASH-5.9.8 = 7656733b316562662026ac82a7c0be41440e16bbf1bdc5447b119e34ff3b86a6
 
 remove_uri_prefix=$(subst git://,,$(subst http://,,$(subst https://,,$(1))))
 sanitize_uri=$(call qstrip,$(subst @,_,$(subst :,_,$(subst .,_,$(subst -,_,$(subst /,_,$(1)))))))
